0bdf3ff9127a2bb8302f0349ca95ef55d8cea85b
Keerthivasan-04 <a.keerthi2004@gmail.com>
2026-01-06 12:58:23 +0530

    feat: add new code

---
+++ ArraySeries/src/Level_4/MoveZeros.java
@@ -0,0 +1,59 @@
+package Level_4;
+
+import java.util.Scanner;
+
+/**
+ * Move all zeros to the end (maintain order)
+ */
+
+public class MoveZeros {
+
+    // --- Validation methods ---
+    private static void validateInputSize(int size) {
+        if(size < 0){
+            throw new IllegalArgumentException("Array size must be positive");
+        }
+        if(size == 0){
+            throw new IllegalArgumentException("Array size must be greater than zero");
+        }
+    }
+
+    private static void validateArray(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            throw new IllegalArgumentException("Array must not be empty");
+        }
+    }
+
+    // --- Business Methods ---
+    private static void moveZerostoLeft(int[] arr, int size) {
+        validateArray(arr);
+        int left = 0;
+
+        for(int i = 0; i < size; i++){
+            if(arr[i] != 0){
+                int temp = arr[i];
+                arr[i] = arr[left];
+                arr[left] = temp;
+                left++;
+            }
+        }
+        for(int num : arr){
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the array size: ");
+
+        int size = sc.nextInt();
+        validateInputSize(size);
+
+        int[] arr = new int[size];
+        System.out.println("Enter the array elements: ");
+        for(int i = 0; i < size; i++){
+            arr[i] = sc.nextInt();
+        }
+        moveZerostoLeft(arr,size);
+    }
+}
