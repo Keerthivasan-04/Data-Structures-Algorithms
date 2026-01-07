def121de9d1ab7bd5401e3d551dccd6e226ab6a7
Keerthivasan-04 <a.keerthi2004@gmail.com>
2026-01-07 10:09:53 +0530

    feat: add new coding question

--- ArraySeries/src/Level_4/NegativetoLeft.java
+++ ArraySeries/src/Level_4/NegativetoLeft.java
@@ -1,5 +1,7 @@
 package Level_4;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -25,21 +27,70 @@ public class NegativetoLeft {
     }
 
     // --- Business Methods ---
-    private static void moveZerostoLeft(int[] arr, int size) {
-        validateArray(arr);
-        int left = 0;
-
-        for(int i = 0; i < size; i++){
-            if(arr[i] != 0){
-                int temp = arr[i];
-                arr[i] = arr[left];
-                arr[left] = temp;
-                left++;
-            }
-        }
+    private static void displayArray(int[] arr){
         for(int num : arr){
             System.out.print(num + " ");
         }
+        System.out.println();
+    }
+
+    private static void moveNegativetoLeft_ExtraSpace(int[] arr, int size) {
+        /**
+         * Time COmplexity : O(n)
+         * Space Complexity : O(n)
+         */
+
+        validateArray(arr);
+        List<Integer> negativeNumber = new ArrayList<>();
+        List<Integer> positiveNumber = new ArrayList<>();
+
+        for(int i = 0; i < size; i++){
+            if(arr[i] < 0){
+                negativeNumber.add(arr[i]);
+            }
+            else{
+                positiveNumber.add(arr[i]);
+            }
+        }
+
+        for(int i = 0; i < negativeNumber.size(); i++){
+            arr[i] = negativeNumber.get(i);
+        }
+        int j = 0;
+        for(int i = negativeNumber.size(); i < arr.length; i++){
+            arr[i] = positiveNumber.get(j);
+            j++;
+        }
+
+        System.out.println("Move negative elements to left - Extra Space: ");
+        displayArray(arr);
+    }
+
+    private static void moveNegativetoLeft_ExtraTime(int[] arr, int size) {
+        /**
+         * Time COmplexity : O(n^2)
+         * Space Complexity : O(1)
+         */
+
+        int pos = 0;
+
+        for(int i = 0; i < size; i++){
+            if(arr[i] < 0){
+                int temp = arr[i];
+                int j = i;
+
+                while(j > pos){
+                    arr[j] = arr[j - 1];
+                    j--;
+                }
+                arr[pos] = temp;
+                pos++;
+            }
+        }
+
+        System.out.println("Move negative elements to left - Extra Time: ");
+        displayArray(arr);
+
     }
 
     public static void main(String[] args) {
@@ -54,6 +105,8 @@ public class NegativetoLeft {
         for(int i = 0; i < size; i++){
             arr[i] = sc.nextInt();
         }
-        moveZerostoLeft(arr,size);
+
+        moveNegativetoLeft_ExtraSpace(arr,8);
+        moveNegativetoLeft_ExtraTime(arr,8);
     }
 }
